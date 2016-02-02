1c877bac918aecfb8d30a8bc6f94f2e0aad2af44
Jen Wachter <jenleighkelly@gmail.com>
2016-02-02 10:26:12 -0500

    Removed logging

--- src/Composer/FontInstallerInstaller.php
+++ src/Composer/FontInstallerInstaller.php
@@ -10,7 +10,6 @@ class FontInstallerInstaller extends LibraryInstaller
   public function getInstallPath(PackageInterface $package)
   {
     $name = str_replace("johnshopkins/", "", $package->getPrettyName());
-    echo $name . "\n";
     return 'src/assets/fonts/lib/' . $name;
   }
 
